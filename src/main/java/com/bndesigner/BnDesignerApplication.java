a37975c71a52f4f5619bdeddeb00146cfb11d56d
mateussantos15 <mateusrs015@gmail.com>
2025-11-17 17:43:10 -0300

    feat(scaffold): adiciona projeto Spring Boot gerado pelo Spring Initializr

---
+++ src/main/java/com/bndesigner/BnDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.bndesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnDesignerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bndesigner/BnDesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bndesigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnDesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
