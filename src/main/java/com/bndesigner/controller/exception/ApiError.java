9d7d2c59c7f0eff4e071df5f8d534ea6799fbc22
Mateus Santos <99846038+mateussantos15@users.noreply.github.com>
2026-01-15 19:07:24 -0300

    Revert "feat(usuario): endpoint de criação e tratamento global de erros (#10)"
    This reverts commit a64df779a909085df704c66468b65778c620a2db.

--- src/main/java/com/bndesigner/controller/exception/ApiError.java
+++
@@ -1,8 +0,0 @@
-package com.bndesigner.controller.exception;
-
-import java.time.OffsetDateTime;
-
-public record ApiError(int status,
-		String error,
-		String message,
-		OffsetDateTime timestamp) {}
